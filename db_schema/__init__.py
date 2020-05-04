f2fef6353abcd6052f41308be51dc6a411f579bf
Hristiyana <hpmalinova@github.com>
2020-05-04 21:42:48 +0300

    Move create tables to db_schema

--- db_schema/__init__.py
+++ db_schema/__init__.py
@@ -1,1 +1,4 @@
 from .users import CREATE_USERS
+from .movies import CREATE_MOVIES
+from .projections import CREATE_PROJECTIONS
+from .reservations import CREATE_RESERVATIONS
--- users.py
+++
@@ -1,8 +0,0 @@
-# TODO: Hash the passwords
-CREATE_USERS = '''
-    CREATE TABLE IF NOT EXISTS users (
-        id integer PRIMARY KEY NOT NULL,
-        email varchar(50) UNIQUE,
-        password varchar(50)
-    );
-'''
