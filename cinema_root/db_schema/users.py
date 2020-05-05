de16f1a782416d4c063916b0f3499ff944bae836
Hristiyana <hpmalinova@github.com>
2020-05-05 13:46:30 +0300

    Add type column in Users

--- cinema_root/db_schema/users.py
+++ cinema_root/db_schema/users.py
@@ -1,8 +1,9 @@
 # TODO: Hash the passwords
 CREATE_USERS = '''
     CREATE TABLE IF NOT EXISTS users (
-        id integer PRIMARY KEY NOT NULL,
-        email varchar(50) UNIQUE,
-        password varchar(50)
+        id INTEGER PRIMARY KEY NOT NULL,
+        email VARCHAR(50) UNIQUE NOT NULL,
+        password VARCHAR(50) NOT NULL,
+        user_type VARCHAR(10) NOT NULL CHECK(user_type = 'Admin' or user_type = 'Client')
     );
 '''
