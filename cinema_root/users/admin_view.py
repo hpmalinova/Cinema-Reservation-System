b6cb792cc5c36a69e5c24fe9e1af7b02413fcd44
TsvetomirTsvetkov <cvetomircvetkov24@gmail.com>
2020-05-05 23:32:54 +0300

    Added base AdminView

--- cinema_root/users/admin_view.py
+++ cinema_root/users/admin_view.py
@@ -17,10 +17,62 @@
 # delete
 # update/time/type
 
+from .controllers import UserController
+
+
 class AdminView:
     def __init__(self, user):
         self.user = user
+        self.controller = UserController()
         self.welcome()
 
     def welcome(self):
-        print(f'Welcome to HackCinema, {self.user.name}')
+        print(f'Welcome to HackCinema, {self.user.email}')
+        self.execute_command('help')
+
+        command = self.get_input('>> Your command: ')
+
+        while command != 'exit':
+            self.execute_command(command)
+            command = self.get_input('>> Your command: ')
+
+        print('Goodbye!')
+
+    # @staticmethod
+    def execute_command(self, command):
+        all_commands = {'help': self.show_commands, 'get_all_users': self.get_all_users}
+
+        if command in all_commands:
+            all_commands[command]()
+        else:
+            print(f'Unknown command: {command}. Try again!')
+            return False
+
+    def get_all_users(self):
+        user_models = self.controller.get_all_users()
+        for user in user_models:
+            print(user.user_id, user.email, user.password, user.user_type)
+
+    @staticmethod
+    def show_commands():
+        print('# Users')
+        print('- get_user <id>')
+        print('- get_all_users')  # DONE
+        print('- change_type_user <id> <user_type>')
+        print('- delete_user <id>\n')
+        print('# Movie')
+        print('- add_movie')
+        print('- show_all_movies')
+        print('- delete_movie\n')
+        print('# Projections')
+        print('- add_projection')
+        print('- update_projection')
+        print('- delete_projection')
+        print('# help')
+
+    @staticmethod
+    def get_input(msg):
+        var = input(msg)
+        while not var:
+            var = input(msg)
+        return var
--- cinema_root/users/users_gateway.py
+++ cinema_root/users/users_gateway.py
@@ -8,10 +8,9 @@ class UserGateway:
         self.model = UserModel
         self.db = Database()
 
-    def create(self, *, email, password):
+    def add_user(self, *, email, password):  # DONE
         self.model.validate(email, password)
 
-        print('email', email)
         hashed_password = self.model.hash_password(password)
 
         # Create User in DB
@@ -25,30 +24,14 @@ class UserGateway:
         get_id_query = f'SELECT id FROM users WHERE email= \'{email}\';'
         self.db.cursor.execute(get_id_query)
 
-        user_id = self.db.cursor.fetchone()[0]  # CHECK TYPE
+        user_id = self.db.cursor.fetchone()[0]
 
         self.db.connection.commit()
-
-        if user_id:
-            print('user_id', user_id)
-        else:
-            print('no user id')
 
         return self.model(user_id=user_id, email=email,
                           password=hashed_password, user_type='Client')
 
-    def get_all_users(self):
-        self.db.cursor.execute(SELECT_ALL_USERS)
-        raw_users = self.db.cursor.fetchall()
-
-        all_users = []
-        for raw_user in raw_users:
-            all_users.append(self.model(user_id=raw_user[0], email=raw_user[1],
-                                        password=raw_user[2], user_type=raw_user[3]))
-
-        return all_users
-
-    def get(self, *, email, password):
+    def login(self, *, email, password):  # DONE
         # Get User from FB
         get_user_query = f'SELECT * FROM users WHERE email=\'{email}\';'
         self.db.cursor.execute(get_user_query)
@@ -57,13 +40,58 @@ class UserGateway:
         self.db.connection.commit()
 
         if raw_user:
-            print(raw_user[0], ' ', raw_user[1], ' ', raw_user[2], ' ', raw_user[3])
             hashed_password = self.model.hash_password(password)
             if hashed_password == raw_user[2]:
-                print('here')
                 return self.model(user_id=raw_user[0], email=raw_user[1],
                                   password=raw_user[2], user_type=raw_user[3])
             else:
                 raise ValueError('Wrong password')
         else:
             raise ValueError('User not found.')
+
+    def get_user(self, *, id):  # DONE
+        # Get User DB
+        get_user_query = f'SELECT * FROM users WHERE id={id};'
+        self.db.cursor.execute(get_user_query)
+        raw_user = self.db.cursor.fetchone()
+        self.db.connection.commit()
+
+        return self.model(raw_user[0], raw_user[1], raw_user[3], raw_user[4])
+
+    def get_all_users(self):  # DONE
+        self.db.cursor.execute(SELECT_ALL_USERS)
+        raw_users = self.db.cursor.fetchall()
+
+        self.db.connection.commit()
+
+        all_users = []
+        for raw_user in raw_users:
+            all_users.append(self.model(user_id=raw_user[0], email=raw_user[1],
+                                        password=raw_user[2], user_type=raw_user[3]))
+
+        return all_users
+
+    def change_type_user(self, *, id, user_type):  # DONE
+        select_user_query = f'SELECT * FROM users WHERE id ={id};'
+        self.db.cursor.execute(select_user_query)
+        raw_user = self.db.cursor.fetchone()
+
+        if raw_user:
+            delete_user_query = f'UPDATE users SET user_type={user_type} WHERE id={id};'
+            self.db.cursor.execute(delete_user_query)
+            self.db.connection.commit()
+        else:
+            raise Exception('User not found.')
+
+    def delete_user(self, *, id):
+        # Delete User from DB
+        select_user_query = f'SELECT * FROM users WHERE id ={id};'
+        self.db.cursor.execute(select_user_query)
+        raw_user = self.db.cursor.fetchone()
+
+        if raw_user:
+            delete_user_query = f'DELETE FROM users WHERE id={id};'
+            self.db.cursor.execute(delete_user_query)
+            self.db.connection.commit()
+        else:
+            raise Exception('User not found.')
