2cc78e8b02a0d26b2a50290f56318c11eca5e725
Hristiyana <hpmalinova@github.com>
2020-05-07 16:08:39 +0300

    Move queries to projection_queries

--- cinema_root/projections/projection_queries.py
+++ cinema_root/projections/projection_queries.py
@@ -2,3 +2,37 @@ SELECT_ALL_PROJECTIONS = '''
     SELECT *
         FROM projections
 '''
+
+CREATE_PROJECTION = '''
+    INSERT INTO projections (movie_id, p_type, p_date, p_time)
+        VALUES (?, ?, ?, ?);
+'''
+
+GET_PROJECTION_ID = '''
+    SELECT p_id
+        FROM projections
+        WHERE movie_id=? and p_type=?
+            and p_date=? and p_time=?;
+'''
+DELETE_PROJECTION_BY_ID = '''
+    DELETE FROM projections
+        WHERE p_id=?;
+'''
+
+GET_PROJECTION_BY_ID = '''
+    SELECT p_id
+        FROM projections
+        WHERE p_id=?;
+'''
+
+UPDATE_PROJECTION = '''
+    UPDATE projections
+        SET ?=?
+        WHERE p_id=?;
+'''
+
+GET_PROJECTION_BY_MOVIE_ID = '''
+    SELECT *
+        FROM projections
+        WHERE movie_id=?;
+'''
--- cinema_root/projections/projections_gateway.py
+++ cinema_root/projections/projections_gateway.py
@@ -1,5 +1,6 @@
 from cinema_root.db import Database
-from .projection_queries import SELECT_ALL_PROJECTIONS
+from .projection_queries import (SELECT_ALL_PROJECTIONS, CREATE_PROJECTION, GET_PROJECTION_ID, DELETE_PROJECTION_BY_ID,
+                                 GET_PROJECTION_BY_ID, UPDATE_PROJECTION, GET_PROJECTION_BY_MOVIE_ID)
 
 
 class ProjectionGateway:
@@ -9,30 +10,17 @@ class ProjectionGateway:
     def get_all_projections(self):
         self.db.cursor.execute(SELECT_ALL_PROJECTIONS)
         raw_projections = self.db.cursor.fetchall()
-
         self.db.connection.commit()
 
         return raw_projections
 
     def add_projection(self, movie_id, p_type, p_date, p_time):
         # Create Projection in DB
-        create_query = '''
-            INSERT INTO projections (movie_id, p_type, p_date, p_time)
-            VALUES (?, ?, ?, ?);
-        '''
-        self.db.cursor.execute(create_query, (movie_id, p_type, p_date, p_time))
+        self.db.cursor.execute(CREATE_PROJECTION, (movie_id, p_type, p_date, p_time))
 
         # Get Projection ID from DB
-        get_id_query = f'''
-        SELECT p_id
-            FROM projections
-            WHERE movie_id= \'{movie_id}\' and p_type=\'{p_type}\'
-                and p_date= \'{p_date}\' and p_time=\'{p_time}\';
-        '''
-        self.db.cursor.execute(get_id_query)
-
+        self.db.cursor.execute(GET_PROJECTION_ID, (movie_id, p_type, p_date, p_time))
         p_id = self.db.cursor.fetchone()[0]
-
         self.db.connection.commit()
 
         print(f'Successfully created projection with id={p_id}')
@@ -46,22 +34,13 @@ class ProjectionGateway:
             return
 
         # Delete Projection in DB by id
-        delete_query = f'''
-            DELETE FROM projections
-                WHERE p_id= \'{p_id}\'
-        '''
-        self.db.cursor.execute(delete_query)
+        self.db.cursor.execute(DELETE_PROJECTION_BY_ID, (p_id,))
         self.db.connection.commit()
 
         print(f'Projection with id: {p_id} was successfully deleted.')
 
     def get_projection_id(self, p_id):
-        get_id_query = f'''
-        SELECT p_id
-            FROM projections
-            WHERE p_id= \'{p_id}\';
-        '''
-        self.db.cursor.execute(get_id_query)
+        self.db.cursor.execute(GET_PROJECTION_BY_ID, (p_id, ))
         projection_id = self.db.cursor.fetchone()[0]
         self.db.connection.commit()
 
@@ -73,23 +52,13 @@ class ProjectionGateway:
         if not projection_id:
             raise ValueError('You can`t update non existing projection.')
 
-        update_query = f'''
-            UPDATE projections
-                SET {to_upd} = \'{new_value}\'
-                WHERE p_id= \'{p_id}\'
-        '''
-        self.db.cursor.execute(update_query)
+        self.db.cursor.execute(UPDATE_PROJECTION, (to_upd, new_value, p_id))
         self.db.connection.commit()
 
         print(f'Projection with id: {p_id} was successfully updated.')
 
     def get_projections_by_movie_id(self, movie_id):
-        select_query = f'''
-        SELECT *
-            FROM projections
-            WHERE movie_id= \'{movie_id}\';
-        '''
-        self.db.cursor.execute(select_query)
+        self.db.cursor.execute(GET_PROJECTION_BY_MOVIE_ID, (movie_id,))
         raw_projections = self.db.cursor.fetchall()
         self.db.connection.commit()
 
