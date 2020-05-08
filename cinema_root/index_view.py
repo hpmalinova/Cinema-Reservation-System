ff1eec94af8060f6bedc6050bff33bb94eb5a2a2
TsvetomirTsvetkov <cvetomircvetkov24@gmail.com>
2020-05-08 13:29:16 +0300

    Fixed return to main menu

--- cinema_root/index_view.py
+++ cinema_root/index_view.py
@@ -2,26 +2,39 @@ from cinema_root.users.views import UserViews
 from .utils import HACKCINEMA
 import os
 
+ALL_COMMANDS = ['login', 'signup', 'exit']
+
 
 def welcome():
-    os.system('clear')
-    print(HACKCINEMA)
-    print('[Welcome to HackCinema!]\n')
-    all_commands = ['login', 'signup', 'exit']
-    command = input('Choose a command:\n[-] login\n[-] signup\n\n[-] exit\n\nInput: ')
+    print_hackcinema()
+
     user_views = UserViews()
 
-    while command not in all_commands:
+    command = input_command()
+    while command != 'exit':
+        if command == 'login':
+            os.system('clear')
+            user_views.login()
+        elif command == 'signup':
+            os.system('clear')
+            user_views.signup()
+        print_hackcinema()
+        command = input_command()
+        os.system('clear')
+
+
+def input_command():
+    command = input('Choose a command:\n[-] login\n[-] signup\n\n[-] exit\n\nInput: ')
+    while command not in ALL_COMMANDS:
         os.system('clear')
         print(HACKCINEMA)
         print('Welcome to HackCinema!\n')
         print(f'Your last command ({command}) was not recognized.\n')
         command = input('Choose a command:\n[-] login\n[-] signup\n\n[-] exit\n\nInput: ')
+    return command
 
-    if command == 'login':
-        os.system('clear')
-        return user_views.login()
-    elif command == 'signup':
-        os.system('clear')
-        return user_views.signup()
+
+def print_hackcinema():
     os.system('clear')
+    print(HACKCINEMA)
+    print('[Welcome to HackCinema!]\n')
